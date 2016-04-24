0a635798e60a63c4915e80a29a02d2d8f9075747
Tatyana Fursova <mouse@bk.ru>
2016-04-24 16:27:31 +0300

    Plot4

--- Plot4.R
+++ Plot4.R
@@ -22,6 +22,7 @@ lines(a1$DT, a1$Sub_metering_2, col="red")
 lines(a1$DT, a1$Sub_metering_3, col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1,1,1), lwd=c(2,2,2), col=c("black", "red", "blue"), bty="n")
 a1$Global_reactive_power<-as.numeric(a1$Global_reactive_power)
+plot(a1$Global_reactive_power~a1$DT, type='l', xlab="datetime", ylab="Global_reactive_power")
 
 dev.copy(png, 'plot2.png')
 dev.off()
